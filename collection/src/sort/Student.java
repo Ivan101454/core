a5ea2934dc0666a9f593a86f3fe7c9dbf4401b25
Ivan Sapozhnikov <ivan101454@gmail.com>
2024-04-20 09:04:17 +0300

    Add function sort by age

---
+++ collection/src/sort/Student.java
@@ -0,0 +1,52 @@
+package sort;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double averageScore;
+
+    public Student(String firstName, String lastName, int age, double averageScore) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.averageScore = averageScore;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getAverageScore() {
+        return averageScore;
+    }
+
+    public void setAverageScore(double averageScore) {
+        this.averageScore = averageScore;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ collection/src/sort/Test.java
@@ -0,0 +1,22 @@
+package sort;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Student> students = Arrays.asList(new Student("Ivan", "Ivanovich", 20, 9.8),
+                new Student("Petr", "Petrovich", 35, 6.3),
+                new Student("Gennadiy", "Sidorovich", 37, 7.8),
+                new Student("Weronika", "Lamochkina", 36, 9.9),
+                new Student("Sidor", "Sidorovich", 25, 5.4));
+
+        getStudent(students);
+    }
+
+    private static void getStudent(List<Student> students) {
+        Collections.sort(students, new ComparatorAge());
+        System.out.println(students);
+    }
+}
